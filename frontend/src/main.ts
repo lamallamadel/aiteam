eb360674656b1cc9cf70be3d3ba2dec68def66c3
Adel Lamallam <lamallam.adel@gmail.com>
2026-02-23 15:37:02 +0100

    Add sync check and improve compiler options

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,5 +2,7 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
 
+console.log('ATLASIA_DOCKER_SYNC_CHECK_V3');
+
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
